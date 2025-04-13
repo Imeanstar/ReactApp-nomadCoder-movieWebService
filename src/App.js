877d03de0192ded78e0e93682ed9a5dc199b671c
mike4403 <mike4404@naver.com>
2025-04-13 22:23:27 +0900

    movie API받아와서 listing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 //실행시 npm start 를 cmd에 입력
+import React from "react";
 import { useState, useEffect } from "react";
 
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+    // fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+    // .then((response) => response.json())
+    // .then((json) => {
+    //   setMovies(json.data.movies);
+    //   setLoading(false);
+    // });
+  }
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, [])
-  return(
-    <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => <li>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>)}
-      </ul>
-    </div>
-  )
+    getMovies();
+  }, []);
+  // console.log(movies);
+  return <div>
+    {loading ? <h1>loading...</h1> : (
+      <div>{
+        movies.map(movie => 
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+          </ul>
+        </div>
+        )}
+      </div>)
+    }
+  </div>
 }
 
 export default App;
