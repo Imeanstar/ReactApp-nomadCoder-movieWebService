794b5c9f19f2ef5818e279c79f1873a8db18531f
mike4403 <mike4404@naver.com>
2025-04-24 12:56:59 +0900

    App.js의 내용 Home.js로 이식

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,38 @@
 import App from "./../App";
 
 function Home() {
-
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() => {
+        const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+        // fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
+        // .then((response) => response.json())
+        // .then((json) => {
+        //   setMovies(json.data.movies);
+        //   setLoading(false);
+        // });
+    }
+    useEffect(() => {
+        getMovies();
+    }, []);
+    // console.log(movies);
+    return (
+    <div>
+        {loading ? <h1>loading...</h1> : (
+        <div>{
+            movies.map(movie => 
+            <Movie
+                key={movie.id} 
+                medium_cover_image={movie.medium_cover_image} 
+                title={movie.title} 
+                summary ={movie.summary}
+                genres={movie.genres}
+            />
+            )}
+        </div>)
+        }
+    </div>)
 }
