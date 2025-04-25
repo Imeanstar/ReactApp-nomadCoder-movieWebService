08abba798b8d615ba27c6b43eac204ada3edc750
mike4403 <mike4404@naver.com>
2025-04-25 15:06:16 +0900

    App.js에 라우터구조 형성

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Movie from "./components/Movie";
+import Home from "./routes/Home.js";
+import Detail from "./routes/Detail.js";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return null;
+  return <Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
--- src/components/Moive.js
+++ src/components/Moive.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({key, medium_cover_image, title, summary, genres}){
     return 
     <div id={key}>
         <img src={medium_cover_image} />
-        <h2>{title}</h2>
+        <h2>
+            <Link to="/movie">{title}</Link>
+        </h2>
         <p>{summary}</p>
         <ul>
         {genres.map(genre => <li key={genre}>{genre}</li>)}
